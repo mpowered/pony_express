afe194f660d79e527e443c91694b0af753ed3387
Gary Greyling <greyling.gary@gmail.com>
2018-12-04 21:51:34 +0200

    update sidekiq args to support sidekiq 4

--- app/workers/pony_express_sender_worker.rb
+++ app/workers/pony_express_sender_worker.rb
@@ -1,9 +1,7 @@
 class PonyExpressSenderWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: "#{PonyExpress::Config.queue_prefix}_pony_express_#{Rails.env}",
-                  unique: true, 
-                  retry: 3
+  sidekiq_options queue: "#{PonyExpress::Config.queue_prefix}_pony_express_#{Rails.env}", lock: :until_executed, on_conflict: :replace, retry: 3
 
   def perform(recipients, message, params)
     PonyExpress.to(*recipients).send_pony(message, params)
